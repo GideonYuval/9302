d9a26ff3f3561a437d26f7786feb806b9f02afaa
Gideon Yuval <gideonyuval@gmail.com>
2024-12-20 14:58:07 +0200

    fixed static

--- 9302/Program.cs
+++ 9302/Program.cs
@@ -20,6 +20,7 @@ namespace _9302
             return q;
         }
 
+        //1
         //get a queue of ints
         //count how many numbers in q have most significant digit equal least significant digit
         //example: q = [1451, 123, 7827], return 2
@@ -28,37 +29,41 @@ namespace _9302
             return 0;
         }
 
+        //2
         //נגדיר מספר צְמּוד-עשר כמספר שיש לו לפחות שתי ספרות צמודות שסכומן בדיוק 10
         //לדוגמה - 4550 הוא צְמּוד-עשר, 3456 אינו צְמּוד-עשר
         //כתוב פעולה המקבלת שני תורים של מספרים טבעיים
         //הפעולה תחזיר 1 אם לתור הראשון יש יותר צמודי-עשר, 2 אחרת
-        public static int CountDoubleTen(Queue<int> q1, Queue<int> q2)
+        static int CountDoubleTen(Queue<int> q1, Queue<int> q2)
         {
             return 0;
         }
 
+        //3
         //get a queue of chars
         //return a queue of char sequence lengths
         //example: q = ['(', '(', 4, 'A','A','A','%','%','%'] -> return: head[2,1,3,3]
-        public Queue<int> CharSeq (Queue<char> q)
+        static Queue<int> CharSeq (Queue<char> q)
         {
             return null;
 
         }
 
+        //4
         //get a queue of doubles
         //return a queue of doubles, each double is the sum of positive sequences
         //example: q=[5.1, -2.2, -6.0, 0.5, 4.5, 2.0, -3.6, 2.2, 1.5], return: head[5.1, 7.0, 3.7]
-        public Queue<int> SumPosSeq(Queue<double> q)
+        static Queue<int> SumPosSeq(Queue<double> q)
         {
             return null;
 
         }
 
+        //5
         //get a queue of ints and int k
         //return a queue of ints which includes for each k-digit sequence, the length of sequence
         //example: k=3, q=[987, 12, 100, 75, 770, 880, 990, 35, 200, 123], return: head[1,1,3,2]
-        public Queue<int> KLenSeq(Queue<int> q)
+        static Queue<int> KLenSeq(Queue<int> q)
         {
             return null;
         }
